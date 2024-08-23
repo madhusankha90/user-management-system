db95ab7337d1c5c63f5ec2ff98a13cdf69fb56fe
madhusankha90 <madhusankha.networking@gmail.com>
2024-08-24 03:25:22 +0530

    update app.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App(){
+    return(
+        <div className="">
+            <header className="lex justify-center items-center">
+                <h1 className="font-extrabold text-5xl text-center">Welcome to this</h1>
+<button className="border-2 border-black rounded-md px-4 py-2 bg-blue-300 cursor-pointer hover:bg-blue-400 active:bg-blue-500">Users</button>
+            </header>
+
+        </div>
+    )
+}
+
+export default App;
