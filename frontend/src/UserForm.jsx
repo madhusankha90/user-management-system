f6b35854950eab3079d811413945952963eb0e2a
madhusankha90 <madhusankha.networking@gmail.com>
2024-08-28 01:11:47 +0530

    create UserForm.jsx

---
+++ frontend/src/UserForm.jsx
@@ -0,0 +1,10 @@
+const UserForm = (props) => {
+
+    return(
+        <div>
+            
+        </div>
+    );
+
+}
+export default UserForm;
--- frontend/src/Users.jsx
+++ frontend/src/Users.jsx
@@ -1,4 +1,4 @@
-const Users = () => {
+const Users = () =>{
 
 }
 
