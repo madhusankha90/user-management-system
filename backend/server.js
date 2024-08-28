9d67a1ca7b0c9030658d52bdd3428743dff24fa3
madhusankha90 <madhusankha.networking@gmail.com>
2024-08-29 04:18:59 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,29 @@
-const app = require('./app')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const app = express();
 
 
 
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const URI = 'mongodb://localhost:27017/projectOne'
+
+const connect = async ()=>{
+    try {
+        await mongoose.connect(URI);
+        console.log("MongoDB connected");
+        
+    } catch (error) {
+        console.log("MongoDB not connected",error);
+        
+    }
+    
+}
+connect();
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
