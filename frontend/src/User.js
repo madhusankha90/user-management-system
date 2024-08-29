6d15eb0314b944499933bd1867096f72581d68fb
madhusankha90 <madhusankha.networking@gmail.com>
2024-08-30 03:44:08 +0530

    config get method using axios

--- frontend/src/User.js
+++ frontend/src/User.js
@@ -1,8 +1,27 @@
-import UserForm from "./UserForm";
+import { useEffect, useState } from "react";
 import UserTable from "./UserTable";
-import users from './userData'
+import axios, { Axios } from 'axios';
 
 const User = () =>{
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=>{
+        getUsers();
+    },[]);
+
+    const getUsers = () => {
+        axios.get('http://localhost:5000/api/users')
+            .then(response =>{
+                setUsers(response.data?.response || [])
+                
+            })
+            .catch(error=>{
+                console.error("Axios Error:", error);
+            })
+    }
+
+
     return(
         <div>
             <UserTable rows={users}/>
